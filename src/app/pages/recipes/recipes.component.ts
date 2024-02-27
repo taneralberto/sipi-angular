0b89def2af3af8633f8497661bd168467c1e6599
taneralberto <taneralberto@gmail.com>
2024-02-27 16:43:01 -0400

    recipe list convert to drag and drop

--- src/app/pages/recipes/recipes.component.ts
+++ src/app/pages/recipes/recipes.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { IRecipe } from '../../models/recipe.interface';
 import { CommonModule, NgFor } from '@angular/common';
 import { FormControl, FormGroup, FormsModule } from '@angular/forms';
+import {CdkDragDrop, CdkDrag, CdkDropList, moveItemInArray} from '@angular/cdk/drag-drop';
+
 
 @Component({
   selector: 'app-recipes',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, CdkDropList, CdkDrag],
   templateUrl: './recipes.component.html',
   styleUrl: './recipes.component.scss'
 })
@@ -18,7 +20,6 @@ export class RecipesComponent {
     description: '',
     ingredients: '',
   }
-
   public recipes: IRecipe[] = [
     {
       id: 1,
@@ -31,8 +32,24 @@ export class RecipesComponent {
       name: 'Recipe 2',
       description: 'Recipe 2 description',
       ingredients: new Set<string>(['ingredient 4', 'ingredient 5', 'ingredient 6']),
-    }
+    },
+    {
+      id: 3,
+      name: 'Recipe 3',
+      description: 'Recipe 3 description',
+      ingredients: new Set<string>(['ingredient 7', 'ingredient 2', 'ingredient 3']),
+    },
+    {
+      id: 4,
+      name: 'Recipe 4',
+      description: 'Recipe 2 description',
+      ingredients: new Set<string>(['ingredient 4', 'ingredient 5', 'ingredient 6']),
+    },
   ];
+
+  drop(event: CdkDragDrop<IRecipe[]>) {
+    moveItemInArray(this.recipes, event.previousIndex, event.currentIndex);
+  }
 
   public add(event: Event) {
     event.preventDefault();
